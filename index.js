0d40f8b0c053431d51edc3be4d17d4ae6ebe1397
Sam Kitchen <samkitchen94@gmail.com>
2019-01-13 11:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function logShout() {
 function logWhisper() {
   console.log("HELLO".toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-  return ("I can't hear you!")
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
